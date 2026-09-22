b58c75b70d61e8540b6c03d8a1c5080d986c824c
Duy Nguyễn <duy796017@gmail.com>
2026-09-22 09:59:07 +0700

    fix code

--- ex4.py
+++ ex4.py
@@ -1,6 +1,8 @@
+def Tinh(a,b,c):
+  print("tổng", a+b+c)
+  print("tích", a*b*c)
+  print("trung bình", (a+b+c)/3)
 a=int(input("nhập một số nguyên bất kỳ cho a:"))
 b=int(input("nhập một số nguyên bất kỳ cho b:"))
 c=int(input("nhập một số nguyên bất kỳ cho c:"))
-print("tổng của a, b và c là:", a+b+c)
-print("tích của a, b và c là:", a*b*c)
-print("giá trị trung bình là:", (a+b+c)/3)
+Tinh(a,b,c)
