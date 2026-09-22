05e558e15e2d17e535d455a4a9990e9f7008ef89
Duy Nguyễn <duy796017@gmail.com>
2026-09-22 10:04:32 +0700

    fix code

--- ex5.py
+++ ex5.py
@@ -1,7 +1,6 @@
+def Khoiluong(t, p, l):
+  print("tổng khối lượng là", (((t*20+p)*32+l)*13.3)//1000, "KG và",(((t*20+p)*32+l)*13.3)%1000 ,"G")
 t=float(input("nhập khối lượng (talent):"))
 p=float(input("nhập khối lượng (pound):"))
 l=float(input("nhập khối lượng (lot):"))
-S=(((t*20+p)*32+l)*13.3)
-kg=(S//1000)
-g=(S%1000)
-print("tổng khối lượng là: " ,kg ,"KG và", g ,"G")
+Khoiluong(t, p, l)
