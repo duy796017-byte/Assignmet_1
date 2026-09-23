8d464a273203d8b56ed726a42163fa7fe167dfa6
Duy Nguyễn <duy796017@gmail.com>
2026-09-23 20:20:03 +0700

    Delete ex3.py

--- ex3.py
+++
@@ -1,6 +0,0 @@
-def CV_DT (A,B):
-  print("chu vi hình chữ nhật là:", (A+B)*2)
-  print("diện tích hình chữ nhật là:",A*B)
-A=float(input("nhập chiều dài của hình chữ nhật:"))
-B=float(input("nhập chiều rộng của hình chữ nhật:"))
-CV_DT (A,B)
