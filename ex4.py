e7dd3e0b2010414a77b42d55859f714b676a02c9
Duy Nguyễn <duy796017@gmail.com>
2026-09-23 20:20:13 +0700

    Delete ex4.py

--- ex4.py
+++
@@ -1,8 +0,0 @@
-def Tinh(a,b,c):
-  print("tổng", a+b+c)
-  print("tích", a*b*c)
-  print("trung bình", (a+b+c)/3)
-a=int(input("nhập một số nguyên bất kỳ cho a:"))
-b=int(input("nhập một số nguyên bất kỳ cho b:"))
-c=int(input("nhập một số nguyên bất kỳ cho c:"))
-Tinh(a,b,c)
